6b92752404ee8e799aa9f606171d0421222d3f23
Wonmi <wonmadesign@gmail.com>
2019-02-10 14:45:12 +0900

    Fixed database host

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ const bcrypt = require('bcrypt')
 const db = require('knex')({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: '',
-        password: '',
-        database: 'facetector'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
